72f1594698128a662750deafae191b84e3a4da65
Mirek Kratochvil <exa.exa@gmail.com>
2021-04-14 18:03:11 +0200

    actually add the default antigens and fluorochromes
    oh my

---
+++ R/default_fluorochromes.R
@@ -0,0 +1,33 @@
+# Random set of fluorochromes collected from the internet.
+# It would be much better to have a systematic repository with those.
+# Let us know if you find one. :]
+
+defaultFluorochromes <- c(
+'7AAD', 'Alexa Fluor 350', 'Alexa Fluor 405', 'Alexa Fluor 488', 'Alexa Fluor 532', 'Alexa Fluor 546', 'Alexa Fluor 555', 'Alexa Fluor 561', 'Alexa Fluor 568', 'Alexa Fluor 594',
+'Alexa Fluor 647', 'Alexa Fluor 660', 'Alexa Fluor 680', 'Alexa Fluor 700', 'Alexa Fluor 750', 'Alexa Fluor 790', 'AmCyan', 'Amethyst Orange', 'APC', 'APC-Alexa Fluor 700',
+'APC-Alexa Fluor 750', 'APC-C750', 'APC-Cy5.5', 'APC-Cy7', 'APC-eFluor 780', 'APC-Fire 750', 'APC/Fire 810', 'APC-H7', 'APC-R700', 'APC-Vio 770',
+'BB515', 'BB630', 'BB660', 'BB700', 'BB755', 'BB790', 'BFP', 'BUV395', 'BUV496', 'BUV563',
+'BUV615', 'BUV661', 'BUV737', 'BUV805', 'BV421', 'BV480', 'BV510', 'BV570', 'BV605', 'BV650',
+'BV711', 'BV750', 'BV785', 'BV786', 'BYG584', 'CBD450', 'CBD500', 'CellTrace Blue', 'CellTrace BODIPY', 'CellTrace Calcein Blue AM',
+'CellTrace Calcein Green AM', 'CellTrace Far Red', 'CellTrace Violet', 'CellTrace Yellow', 'CellTracker Green CMFDA', 'CF405L', 'CF405M', 'CF405S', 'CF430', 'CF450',
+'CF488A', 'CF514', 'CF532', 'CF543', 'CF555', 'CF568', 'CF570', 'CF583', 'CF594', 'CF633',
+'CF640R', 'CF647', 'CF660C', 'CF660R', 'CF680', 'CF680R', 'CF700', 'CF750', 'CF770', 'cFluor B515',
+'cFluor B532', 'cFluor B548', 'cFluor B690', 'cFluor BYG575', 'cFluor BYG610', 'cFluor BYG667', 'cFluor BYG680', 'cFluor BYG710', 'cFluor BYG781', 'cFluor R659',
+'cFluor R668', 'cFluor R720', 'cFluor R780', 'cFluor R840', 'cFluor V420', 'cFluor V450', 'cFluor V547', 'cFluor V610', 'cFluor YG584', 'cFluor YG610',
+'CFP', 'CFSE', 'Chromomycin A3', 'Cy3', 'DAPI', 'DRAQ5', 'DRAQ7', 'DsRed', 'DyLight 350', 'DyLight 405',
+'DyLight 550', 'DyLight 650', 'DyLight 800', 'ECD', 'eFluor 450', 'eFluor 455', 'eFluor 506', 'eFluor 520', 'eFluor 570', 'eFluor 615',
+'eFluor 660', 'eFluor 780', 'EGFP', 'EYFP', 'FITC', 'Fixable Viability Stain 450', 'Fixable Viability Stain 510', 'Fixable Viability Stain 520', 'Fixable Viability Stain 570', 'Fixable Viability Stain 575V',
+'Fixable Viability Stain 620', 'Fixable Viability Stain 660', 'Fixable Viability Stain 700', 'Fixable Viability Stain 780', 'GFP', 'Ghost Dye Blue 516', 'Ghost Dye Red 710', 'Ghost Dye Red 780', 'Ghost Dye UV 450', 'Ghost Dye Violet 450',
+'Ghost Dye Violet 510', 'Ghost Dye Violet 540', 'Hoechst 33342', 'Janelia Fluor 646', 'KIRAVIA Blue 520', 'Krome Orange', 'LIVE DEAD Aqua', 'LIVE DEAD Blue', 'LIVE DEAD Far Red', 'LIVE DEAD Green',
+'LIVE DEAD NIR', 'LIVE DEAD Red', 'LIVE DEAD Violet', 'LIVE DEAD Yellow', 'Live-or-Dye 405/452', 'Live-or-Dye 405/545', 'Live-or-Dye 488/515', 'Live-or-Dye 510/550', 'Live-or-Dye 568/583', 'Live-or-Dye 594/614',
+'Live-or-Dye 640/662', 'Live-or-Dye 665/685', 'Live-or-Dye 750/777', 'LSSmOrange', 'mApple', 'mCerulean3', 'mCherry', 'MitoSOX Red', 'MitoSpy Orange CMTMRos', 'MitoTracker Green',
+'MitoTracker Orange', 'mKate', 'Monochlorobimane', 'mOrange', 'mTagBFP2', 'Northern Lights 493', 'Northern Lights 557', 'Northern Lights 637', 'NovaBlue 530', 'NovaBlue 610',
+'NovaBlue 660', 'NovaYellow 570', 'NovaYellow 610', 'NovaYellow 660', 'NucView 488', 'OC515', 'Pacific Blue', 'Pacific Orange', 'PE', 'PE-Alexa Fluor 610',
+'PE-Alexa Fluor 610', 'PE-Alexa Fluor 700', 'PE-CF594', 'PE-Cy5', 'PE-Cy5.5', 'PE-Cy7', 'PE/Dazzle594', 'PE-eFluor 610', 'PE/Fire 640', 'PE/Fire 700',
+'PerCP', 'PerCP-Cy5.5', 'PerCP-eFluor 710', 'PerCP-Vio700', 'PE-Texas Red', 'PE-Vio 615', 'PE-Vio770', 'PI', 'Qdot 605', 'Qdot 655',
+'Qdot 705', 'Qdot 800', 'R718', 'redFluor 710', 'RFP', 'rhod-2 AM', 'Sapphire', 'Spark Blue 550', 'Spark NIR 685', 'Spark Violet 538',
+'Spark YG 570', 'Spark YG 581', 'Spark YG 593', 'Super Bright 436', 'Super Bright 600', 'Super Bright 645', 'Super Bright 702', 'Super Bright 780', 'SYTOX Blue', 'SYTOX Deep Red',
+'SYTOX Green', 'SYTOX Orange', 'Tag-It Violet', 'TAMRA', 'tdTomato', 'TMRE', 'TMRM', 'V450', 'V500', 'Venus',
+'ViaDye Red', 'ViaKrome 405', 'ViaKrome 561', 'ViaKrome 561', 'ViaKrome 638', 'ViaKrome 808', 'Vio515', 'Vio667', 'VioBlue', 'VioBright 667',
+'VioBright FITC', 'VioGreen', 'violetFluor 450', 'violetFluor 500', 'YFP', 'Zombie Aqua', 'Zombie Green', 'Zombie NIR', 'Zombie Red', 'Zombie UV',
+'Zombie Violet', 'Zombie Yellow')
