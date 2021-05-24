ca0da14e4c0518e6cf389cc9fc4c878f066216ba
Mirek Kratochvil <exa.exa@gmail.com>
2021-05-24 20:21:41 +0200

    clean up some minor things

--- R/p_optimize.R
+++ R/p_optimize.R
@@ -1,10 +1,11 @@
 
 menuOptimizer <- function(input,output,session,wsp) {
-  output$menuOptimizer <- shiny::renderUI("optimizer menu!")
+  output$menuOptimizer <- shiny::renderUI("Panel optimizer")
 }
 
 serveOptimizer <- function(input, output, session, wsp) {
   output$uiOptimizer <- shiny::renderUI(shiny::tagList(
-    shiny::h1("Optimize the panel")
+    shiny::h1("Optimize the panel"),
+    "This is work-in-progress."
   ))
 }
